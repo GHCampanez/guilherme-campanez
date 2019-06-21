02868aca26a7f9cfed98295cf291afbdb95ef3f4
GHCampanez <guilhermecampanez@gmail.com>
2019-06-21 13:17:30 -0300

    Updates

---
+++ precache-manifest.2899c1f6f7eb12c450a2dd006bc22a30.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cc9c4478c6201bbeaeb596aa69aced",
+    "url": "/guilherme-campanez/index.html"
+  },
+  {
+    "revision": "5904bead0779cef09719",
+    "url": "/guilherme-campanez/static/css/main.69ca2857.chunk.css"
+  },
+  {
+    "revision": "25d37b445c2ae7792ece",
+    "url": "/guilherme-campanez/static/js/2.5fdfb831.chunk.js"
+  },
+  {
+    "revision": "5904bead0779cef09719",
+    "url": "/guilherme-campanez/static/js/main.97a900d7.chunk.js"
+  },
+  {
+    "revision": "a50fd7fd549e8a7b1093",
+    "url": "/guilherme-campanez/static/js/runtime~main.6640663c.js"
+  },
+  {
+    "revision": "ed9fc61b2dc8023e03cfbf44e6975061",
+    "url": "/guilherme-campanez/static/media/Guilherme.ed9fc61b.pdf"
+  },
+  {
+    "revision": "43129a8ebd469022239d4f68a492bba8",
+    "url": "/guilherme-campanez/static/media/api.43129a8e.jpg"
+  },
+  {
+    "revision": "2eb27cfb9c9d021938147c0bfbc11907",
+    "url": "/guilherme-campanez/static/media/arquitetura.2eb27cfb.jpeg"
+  },
+  {
+    "revision": "2ceca4e67be4cfc950e4f2980f2207c2",
+    "url": "/guilherme-campanez/static/media/foto.2ceca4e6.jpg"
+  },
+  {
+    "revision": "1bc9b3eb6141a37852f9f34735e24b0b",
+    "url": "/guilherme-campanez/static/media/ia.1bc9b3eb.jpg"
+  },
+  {
+    "revision": "25cc64dad91aef39990b08bf8b4d003e",
+    "url": "/guilherme-campanez/static/media/spotify.25cc64da.jpg"
+  },
+  {
+    "revision": "39a064506493a19dfb7c7d9ef830111a",
+    "url": "/guilherme-campanez/static/media/whats.39a06450.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e12d19e57342e772533367397b77fac4.js"
+  "/guilherme-campanez/precache-manifest.2899c1f6f7eb12c450a2dd006bc22a30.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/guilherme-campanez/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
